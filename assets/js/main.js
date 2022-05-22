33804780545261bdc6d86088f9c5d59119c2997b
AndersemReis <andersem.Reis@gmail.com>
2022-05-22 15:36:20 -0400

    atualizando container dos cases2

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,17 +20,5 @@ const scrollReveal = ScrollReveal({
     { interval: 50 }
   );
   
-  const nav = document.querySelector("header .container nav");
   
-  const toggle = document.querySelectorAll(".toggle");
-  
-  for (element of toggle) {
-    element.addEventListener("click", () => {
-      nav.classList.toggle("show");
-    });
-  }
-  
-  const links = document.querySelectorAll(
-    "nav .menu li .menu-item, nav .btn-primary, nav .menu-social li a"
-  );
   
